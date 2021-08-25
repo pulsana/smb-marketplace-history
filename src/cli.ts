a5fff6f4dfdaa96e24061e1e0caf787506ddd670
rob shaw <rob@statik.dev>
2021-08-25 16:34:54 -0400

    test connecting to solana rpc node for last slot/time

--- src/cli.ts
+++ src/cli.ts
@@ -1,1 +1,9 @@
-console.log('cli');
+import { clusterApiUrl, Connection } from '@solana/web3.js';
+
+(async function () {
+  const conn = new Connection(clusterApiUrl('mainnet-beta'), 'confirmed');
+  const slotResult = await conn.getSlot();
+  const blockTimeResult = await conn.getBlockTime(slotResult);
+  const blockTime = new Date(blockTimeResult * 1000);
+  console.log(`Current Slot: ${slotResult} @ ${blockTime}`);
+})();
