f20231dbfc5489441f66942831027ccbcad443e7
rob shaw <rob@statik.dev>
2021-08-25 20:37:04 -0400

    copy over logic for decoding metadata for nft stored by metaplex

--- src/metaplex/classes.ts
+++ src/metaplex/classes.ts
@@ -207,3 +207,89 @@ export interface IMetadataExtension {
     }[];
   };
 }
+
+export const METADATA_SCHEMA = new Map<any, any>([
+  [
+    MasterEditionV1,
+    {
+      kind: 'struct',
+      fields: [
+        ['key', 'u8'],
+        ['supply', 'u64'],
+        ['maxSupply', { kind: 'option', type: 'u64' }],
+        ['printingMint', 'pubkeyAsString'],
+        ['oneTimePrintingAuthorizationMint', 'pubkeyAsString'],
+      ],
+    },
+  ],
+  [
+    MasterEditionV2,
+    {
+      kind: 'struct',
+      fields: [
+        ['key', 'u8'],
+        ['supply', 'u64'],
+        ['maxSupply', { kind: 'option', type: 'u64' }],
+      ],
+    },
+  ],
+  [
+    Edition,
+    {
+      kind: 'struct',
+      fields: [
+        ['key', 'u8'],
+        ['parent', 'pubkeyAsString'],
+        ['edition', 'u64'],
+      ],
+    },
+  ],
+  [
+    Data,
+    {
+      kind: 'struct',
+      fields: [
+        ['name', 'string'],
+        ['symbol', 'string'],
+        ['uri', 'string'],
+        ['sellerFeeBasisPoints', 'u16'],
+        ['creators', { kind: 'option', type: [Creator] }],
+      ],
+    },
+  ],
+  [
+    Creator,
+    {
+      kind: 'struct',
+      fields: [
+        ['address', 'pubkeyAsString'],
+        ['verified', 'u8'],
+        ['share', 'u8'],
+      ],
+    },
+  ],
+  [
+    Metadata,
+    {
+      kind: 'struct',
+      fields: [
+        ['key', 'u8'],
+        ['updateAuthority', 'pubkeyAsString'],
+        ['mint', 'pubkeyAsString'],
+        ['data', Data],
+        ['primarySaleHappened', 'u8'], // bool
+        ['isMutable', 'u8'], // bool
+      ],
+    },
+  ],
+  [
+    EditionMarker,
+    {
+      kind: 'struct',
+      fields: [
+        ['key', 'u8'],
+        ['ledger', [31]],
+      ],
+    },
+  ],
+]);
